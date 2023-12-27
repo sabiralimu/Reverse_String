c47aec0757419a18dcf4036b66d610136b48d42a
sabiralimu <sabiralimu@gmail.com>
2023-12-27 23:32:20 +0600

    remove building function for reverse string

--- app.py
+++ app.py
@@ -1,8 +1,17 @@
 # Input a string
 user_input = input("Enter a string: ")
 
-# Reverse the string
-reversed_string = user_input[::-1]
+# Reverse the string with builtin functions
+#reversed_string = user_input[::-1]
+
+## Reverse the string with for loops
+
+# Initialize an empty string for the reversed input
+reversed_string = ""
+
+# Loop through each character in the input string
+for char in user_input:
+    reversed_string = char + reversed_string
 
 # Output the reversed string
 print("Reversed String:", reversed_string)
