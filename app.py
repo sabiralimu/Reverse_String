6de265bbeec1415772cd6172fbf37a966efe84de
sabira limu <sabiralimu@sabiras-MacBook-Pro.local>
2023-12-27 23:19:17 +0600

    Add string revars code

---
+++ app.py
@@ -0,0 +1,8 @@
+# Input a string
+user_input = input("Enter a string: ")
+
+# Reverse the string
+reversed_string = user_input[::-1]
+
+# Output the reversed string
+print("Reversed String:", reversed_string)
